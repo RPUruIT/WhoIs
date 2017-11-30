eeb77ab19900f6ca7fb4b9ce16aac996ce88c390
CARRASCO\rpintos <rodrigo.pintos@uruit.com>
2017-11-30 10:57:15 -0300

    list view custom with caching strategy

---
+++ WhoIs/WhoIs/WhoIs/CustomComponents/CustomListView.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace WhoIs.CustomComponents
+{
+    public class CustomListView:ListView
+    {
+
+        public CustomListView():base(ListViewCachingStrategy.RecycleElement)
+        {
+            
+            this.ItemSelected += CustomListView_ItemSelected; 
+            
+        }
+
+        private void CustomListView_ItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+            this.SelectedItem = null;
+        }
+    }
+}
