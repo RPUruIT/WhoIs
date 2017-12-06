5db2fca0e1d86313fd14486d9a34f67b50b30f2b
CARRASCO\rpintos <rodrigo.pintos@uruit.com>
2017-12-06 16:18:23 -0300

    I implement the picture taker just for android without using an external component. For iOS is not implemented yet

--- WhoIs/WhoIs/WhoIs.Android/MainActivity.cs
+++ WhoIs/WhoIs/WhoIs.Android/MainActivity.cs
@@ -6,13 +6,15 @@ using Android.Runtime;
 using Android.Views;
 using Android.Widget;
 using Android.OS;
-using Xamarin.Media;
 using Xamarin.Forms;
 using Android.Media;
 using Android.Graphics;
 using Java.IO;
 using System.Threading.Tasks;
 using WhoIs.Configs;
+using Android.Content;
+using Android.Provider;
+using System.Collections.Generic;
 
 [assembly: Dependency(typeof(WhoIs.Droid.MainActivity))]
 namespace WhoIs.Droid
@@ -31,6 +33,8 @@ namespace WhoIs.Droid
 
             ResolveDependencies();
 
+            CreatePicturesDirectory();
+
             LoadApplication(new App());
         }
 
@@ -40,18 +44,36 @@ namespace WhoIs.Droid
             AndroidDependencyContainer.Initialize(DependencyContainer.Container);
         }
 
+        private void CreatePicturesDirectory()
+        {
+            PicturesFiles._picturesDir = new File(Android.OS.Environment.GetExternalStoragePublicDirectory(
+                   Android.OS.Environment.DirectoryPictures), "WhoIS");
+            CreateIfNotExist(PicturesFiles._picturesDir);
+        }
+
+        private File GetFolderInsidePictureDirectory(string folder)
+        {
+            File dir = new File(PicturesFiles._picturesDir, folder);
+            CreateIfNotExist(dir);
+            return dir;
+        }
+
+        private void CreateIfNotExist(File dir) {
+            if (!dir.Exists())
+            {
+                dir.Mkdirs();
+            }
+        }
+
         public void SnapPic(string folder,string name)
         {
             var activity = Forms.Context as Activity;
-
-            var picker = new MediaPicker(activity);
-            var intent = picker.GetTakePhotoUI(new StoreCameraMediaOptions
-            {
-                Name = name + ".jpg",
-                Directory = "WhoIsUsersHunted/" + folder
-            });
-
+            name +=".jpg";
+            Intent intent = new Intent(MediaStore.ActionImageCapture);
+            PicturesFiles._file = new File(GetFolderInsidePictureDirectory(folder), name);
+            intent.PutExtra(MediaStore.ExtraOutput, Android.Net.Uri.FromFile(PicturesFiles._file));
             activity.StartActivityForResult(intent, 1);
+
         }
 
         protected override async void OnActivityResult(int requestCode, Result resultCode, Android.Content.Intent data)
@@ -59,9 +81,7 @@ namespace WhoIs.Droid
             if (resultCode.Equals(Result.Canceled))
                 return;
 
-            var mediaFile = await data.GetMediaFileExtraAsync(Forms.Context);
-
-            string imageFile = mediaFile.Path;
+            string imageFile = PicturesFiles._file.Path;
 
             string thumbnailImageFile = await resizeImage(imageFile);
 
@@ -95,7 +115,11 @@ namespace WhoIs.Droid
             return thumbnailImageFile;
         }
 
-
+        public static class PicturesFiles
+        {
+            public static File _file;
+            public static File _picturesDir;
+        }
     }
 }
 
